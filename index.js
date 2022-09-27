c6e77e202d7e06a98ef5b192a5c428e24f0ed6fe
Akshanabha <84524025+ahqsoftwares@users.noreply.github.com>
2022-09-27 19:46:20 +0530

    Test

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const admin = require("firebase-admin");
+const {getFirestore} = require("firebase-admin/firestore");
+const {getAuth} = require("firebase-admin/auth");
+const {getDocs, collection} = require("firebase/firestore");
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(process.env.TOKEN)),
+  databaseURL: "https://simple-host-api-default-rtdb.firebaseio.com"
+});
+
+const listAllUsers = async(nextPageToken) => {
+  await getAuth(app)
+    .listUsers(1000, nextPageToken)
+    .then(async(listUsersResult) => {
+      listUsersResult.users.forEach((userRecord) => {
+        console.log('user', userRecord.toJSON());
+      });
+      if (listUsersResult.pageToken) {
+        // List next batch of users.
+        await listAllUsers(listUsersResult.pageToken);
+      }
+    })
+    .catch((error) => {
+      console.log('Error listing users:', error);
+    });
+};
+
+const db = getFirestore(app);
+
+(async() => {
+  await listAllUsers();
+  const ref = await db.collection("apps").get();
+  console.log("Working");
+  ref.forEach((doc) => {
+    console.log(doc.id);
+    console.log(doc.data());
+  });
+  console.log("done!");
+})()
