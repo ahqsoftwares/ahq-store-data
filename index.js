1263603e4aa5b6ef7df17ddf15db35306f8bb56b
Akshanabha <84524025+ahqsoftwares@users.noreply.github.com>
2022-09-30 15:07:04 +0530

    changes push database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const admin = require("firebase-admin");
 const fs = require("fs");
+const fetch = require("node-fetch");
 const {getFirestore} = require("firebase-admin/firestore");
 const {getAuth} = require("firebase-admin/auth");
-let users = [],
+let users = {},
 allApps = {},
+unparsedApps = [],
 apps = {},
 homeScreen = [];
 
@@ -19,7 +21,17 @@ const listAllUsers = async(nextPageToken) => {
     .listUsers(1000, nextPageToken)
     .then(async(listUsersResult) => {
       listUsersResult.users.forEach((userRecord) => {
-        users.push(userRecord.toJSON());
+        const {
+          email,
+          photoURL,
+          displayName
+        } = userRecord.toJSON();
+        
+        users[userRecord.uid] = {
+          email,
+          avatar: photoURL,
+          displayName
+        };
       });
       if (listUsersResult.pageToken) {
         // List next batch of users.
@@ -33,20 +45,46 @@ const listAllUsers = async(nextPageToken) => {
 
 const db = getFirestore(app);
 
+// Save Lists
 (async() => {
   await listAllUsers();
   const ref = await db.collection("apps").get();
 
-  ref.forEach((doc) => {
-    console.log(doc.id);
-    console.log(doc.data());
-    allApps[doc.id] = doc.data();
-    apps[doc.data().title] = doc.id;
+  ref.forEach(async(doc) => {
+    unparsedApps.push(doc);
   });
 
+  for (const doc of unparsedApps) {
+    const data = doc.data();
+    
+    let filtered = {
+      ...data,
+      author: {
+        id: data.author,
+        ...users[data.author]
+      }
+    }
+
+    try {
+      const repoReleases = await fetch(`https://api.github.com/repos/${data.repo.author}/${data.repo.location}/releases/latest`).then(async(data)=> await data.json()).then((json) => json.assets);
+      const downloadUri = repoReleases.filter(({name}) => name.endsWith(data.appFinder))[0]["browser_download_url"];
+
+      filtered["download_url"] = downloadUri;
+      
+      allApps[doc.id] = filtered;
+    
+      apps[doc.data().title] = doc.id;
+    } catch (e) {
+      console.log("An App didn't got updated");
+    }
+  }
+
+  // Id - Data Map
   fs.writeFile("./database/apps.json", JSON.stringify(allApps), (err) => {
     console.log(err || "Saved Apps list!");
   });
+
+  // Name - Id map
   fs.writeFile("./database/mapped.json", JSON.stringify(apps), (err) => {
     console.log(err || "Saved Apps name cache!");
   });
